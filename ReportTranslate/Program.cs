9b67b75483c4e20018f549b568fe4c63a5ab1584
Gabriel Paredes <gparedes@digepres.gob.do>
2017-05-19 10:18:29 -0400

    Primer Commit

---
+++ ReportTranslate/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ReportTranslate
+{
+    class Program
+    {
+        static System.Collections.Specialized.StringCollection log = new System.Collections.Specialized.StringCollection();
+
+        static void Main(string[] args)
+        {
+            DirectoryInfo carpeta = new DirectoryInfo(@"C:\Users\gparedes\Desktop\Escritorio 07-2016\Reportes Formulacion\Formulacion 2017vf");
+            DirectoryInfo newCarpeta = new DirectoryInfo(@"C:\Users\gparedes\Desktop\Escritorio 07-2016\Reportes Formulacion\Formulacion 2017vf");
+
+            XmlHelper _helper = new XmlHelper();
+
+            if (!newCarpeta.Exists)
+            {
+                newCarpeta.Create();
+            }
+
+            //Now Create all of the directories
+            //foreach (string dirPath in Directory.GetDirectories(carpeta.FullName, "*",
+            //    SearchOption.AllDirectories))
+            //    Directory.CreateDirectory(dirPath.Replace(carpeta.FullName, newCarpeta.FullName));
+
+            //Translate Oracle to SQL
+            foreach (string newPath in Directory.GetFiles(carpeta.FullName, "*.rdl*", SearchOption.AllDirectories)) {
+                _helper.TranslateReportOracleToSql(newPath, newPath.Replace(carpeta.FullName, newCarpeta.FullName));
+                _helper.ReplaceDatasource(newPath);
+                _helper.ReplaceServer(newPath);
+            }
+
+
+
+            //Replace DataSource
+            //foreach (string newPath in Directory.GetFiles(newCarpeta.FullName, "*.rdl*", SearchOption.AllDirectories))
+            //    _helper.ReplaceDatasource(newPath);
+
+            Console.ReadKey();
+        }
+
+
+
+        static void WalkDirectoryTree(System.IO.DirectoryInfo root, System.IO.DirectoryInfo newRoot)
+        {
+            System.IO.FileInfo[] files = null;
+            System.IO.DirectoryInfo[] subDirs = null;
+
+            // First, process all the files directly under this folder
+            try
+            {
+                XmlHelper _helper = new XmlHelper();
+                
+                files = root.GetFiles("*.rdl");
+
+                foreach (var archivo in files)
+                {
+                    _helper.TranslateReportOracleToSql(archivo.FullName, "");
+                    //Console.WriteLine(string.Format("Archivo:{0} \nDirectorio:{1} \nFullName:{2}", archivo.Name, archivo.DirectoryName, archivo.FullName));
+                }
+            }
+            // This is thrown if even one of the files requires permissions greater
+            // than the application provides.
+            catch (UnauthorizedAccessException e)
+            {
+                // This code just writes out the message and continues to recurse.
+                // You may decide to do something different here. For example, you
+                // can try to elevate your privileges and access the file again.
+                log.Add(e.Message);
+            }
+
+            catch (System.IO.DirectoryNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            if (files != null)
+            {
+                //foreach (System.IO.FileInfo fi in files)
+                //{
+                //    // In this example, we only access the existing FileInfo object. If we
+                //    // want to open, delete or modify the file, then
+                //    // a try-catch block is required here to handle the case
+                //    // where the file has been deleted since the call to TraverseTree().
+                //    Console.WriteLine(fi.FullName);
+                //}
+
+                // Now find all the subdirectories under this directory.
+                subDirs = root.GetDirectories();
+
+                foreach (System.IO.DirectoryInfo dirInfo in subDirs)
+                {
+                    // Resursive call for each subdirectory.
+                  //  WalkDirectoryTree(dirInfo);
+                }
+            }
+        }
+
+
+
+
+    }
+
+
+}
---
+++ ReportTranslate/XmlHelper.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace ReportTranslate
+{
+   public class XmlHelper
+    {
+
+        public void TranslateReportOracleToSql(string RutaArchivo, string RutaSalida)
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(RutaArchivo);
+
+            XmlNamespaceManager nsmgr = new XmlNamespaceManager(xmlDoc.NameTable);
+            nsmgr.AddNamespace("r", "http://schemas.microsoft.com/sqlserver/reporting/2010/01/reportdefinition");
+            nsmgr.AddNamespace("rd", "http://schemas.microsoft.com/SQLServer/reporting/reportdesigner");
+            //nsmgr.AddNamespace("cl", "http://schemas.microsoft.com/sqlserver/reporting/2010/01/componentdefinition");
+
+            //limpiar commandtext
+            XmlNodeList query = xmlDoc.SelectNodes("//r:Query", nsmgr);
+            foreach (XmlNode item in query)
+            {
+                item.InnerXml = item.InnerXml
+                    .Replace("<CommandText>", "<CommandText> ")
+                    .Replace("<CommandText xmlns=\"http://schemas.microsoft.com/sqlserver/reporting/2010/01/reportdefinition\">", "<CommandText xmlns=\"http://schemas.microsoft.com/sqlserver/reporting/2010/01/reportdefinition\"> ");
+            }
+
+            // Console.WriteLine(xmlDoc.OuterXml);
+            XmlNodeList parametros = xmlDoc.SelectNodes("//r:QueryParameter", nsmgr);
+            XmlNodeList CommandTexts = xmlDoc.SelectNodes("//r:CommandText", nsmgr);
+            foreach (XmlNode xmlNode in CommandTexts)
+            {
+                xmlNode.InnerText = xmlNode.InnerText
+
+                    .Replace("||", "+")
+                    .Replace("NVL", "ISNULL")
+                    .Replace(':', '@')
+                    .Replace("substr", "SUBSTRING")
+
+                    //ID VERSION 
+                    .Replace(" v.id_version+'-'", " rtrim(v.id_version)+'-'")
+                    .Replace(" id_version+'-'", " rtrim(id_version)+'-'")
+                    .Replace(" ID_VERSION+'-'", " rtrim(ID_VERSION)+'-'")
+
+                    .Replace(" v.id_version + '-'", " rtrim(v.id_version)+'-'")
+                    .Replace(" id_version + '-'", " rtrim(id_version)+'-'")
+                    .Replace(" ID_VERSION + '-'", " rtrim(ID_VERSION)+'-'")
+
+                    .Replace(" v.id_version +'-'", " rtrim(v.id_version)+'-'")
+                    .Replace(" id_version +'-'", " rtrim(id_version)+'-'")
+                    .Replace(" ID_VERSION +'-'", " rtrim(ID_VERSION)+'-'")
+
+                    .Replace(" v.id_version+ '-'", " rtrim(v.id_version)+'-'")
+                    .Replace(" id_version+ '-'", " rtrim(id_version)+'-'")
+                    .Replace(" ID_VERSION+ '-'", " rtrim(ID_VERSION)+'-'")
+
+                    .Replace("from dual", " ");
+
+                foreach (XmlNode item in parametros)
+                {
+                    var paramName = item.Attributes["Name"].Value;
+                    var paramNameNew = item.Attributes["Name"].Value.Replace(':', '@');
+                    xmlNode.InnerText = xmlNode.InnerText.Replace(paramName, paramNameNew);
+
+                    item.Attributes["Name"].Value = paramNameNew;
+                    // Console.WriteLine(item.Attributes["Name"].Value);
+                }
+                //Console.WriteLine(xmlNode.InnerText);
+                //Console.WriteLine(xmlNode.Attributes["Name"].Value);
+            }
+
+            xmlDoc.Save(RutaSalida);
+        }
+
+
+        public void ReplaceDatasource(string RutaArchivo) {
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(RutaArchivo);
+
+            XmlNamespaceManager nsmgr = new XmlNamespaceManager(xmlDoc.NameTable);
+            nsmgr.AddNamespace("r", "http://schemas.microsoft.com/sqlserver/reporting/2010/01/reportdefinition");
+            nsmgr.AddNamespace("rd", "http://schemas.microsoft.com/SQLServer/reporting/reportdesigner");
+
+            #region MODIFICAR PRIMER DATASOURCE
+            //Creando connection nueva del DataSource
+            XmlElement elem = xmlDoc.CreateElement("DataSourceReference", "http://schemas.microsoft.com/sqlserver/reporting/2010/01/reportdefinition");
+            elem.InnerText = "/Formulacion 2017/Formulacion 2017 Data Source";
+            
+            //Se selecciona el primer DataSOurce
+            XmlNode dataSource = xmlDoc.SelectSingleNode("//r:DataSource", nsmgr);
+
+            if (dataSource != null && dataSource.FirstChild != null)
+            {
+                //Modificando DataSource
+                if (dataSource.FirstChild.LocalName == "DataSourceReference" || dataSource.FirstChild.LocalName == "ConnectionProperties")
+                {
+                    dataSource.ReplaceChild(elem, dataSource.FirstChild);
+                }
+                else
+                {
+                    dataSource.AppendChild(elem);
+                }
+            }
+            #endregion
+
+            xmlDoc.Save(RutaArchivo);
+        }
+
+
+        public void ReplaceServer(string RutaArchivo)
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(RutaArchivo);
+
+            XmlNamespaceManager nsmgr = new XmlNamespaceManager(xmlDoc.NameTable);
+            nsmgr.AddNamespace("r", "http://schemas.microsoft.com/sqlserver/reporting/2010/01/reportdefinition");
+            nsmgr.AddNamespace("rd", "http://schemas.microsoft.com/SQLServer/reporting/reportdesigner");
+
+            #region MODIFICAR PRIMER DATASOURCE
+
+            //Creando connection nueva del DataSource
+            XmlElement elem = xmlDoc.CreateElement("rd:ReportServerUrl", "http://schemas.microsoft.com/SQLServer/reporting/reportdesigner");
+            elem.InnerText = "http://wk12dgpetl01/ReportServer";
+
+
+            //Se selecciona el primer DataSOurce
+            XmlNode servidorUrl = xmlDoc.SelectSingleNode("//rd:ReportServerUrl", nsmgr);
+
+            if (servidorUrl != null)
+            {
+                //Modificando server
+                servidorUrl.InnerText.Replace(servidorUrl.InnerText, elem.InnerText);
+            }
+            else
+            {
+                XmlNode report = xmlDoc.SelectSingleNode("//r:Report", nsmgr);
+
+                report.AppendChild(elem);
+            }
+
+            #endregion
+
+            xmlDoc.Save(RutaArchivo);
+        }
+
+
+
+
+
+        public RDLAttributes ReadFile(string filename)
+        {
+            RDLAttributes rptAtt = new RDLAttributes();
+            List<RDLParam> l = new List<RDLParam>();
+
+            // Create an XmlReader
+            using (XmlReader reader = XmlTextReader.Create(filename))
+            {
+                reader.ReadToFollowing("DataSourceName");
+                rptAtt.DataSourceName = "DataSourceName: " + reader.ReadElementContentAsString();
+
+                reader.ReadToFollowing("CommandText");
+                rptAtt.CommandText = "CommandText: " + reader.ReadElementContentAsString();
+
+                string temp = "";
+                reader.ReadToFollowing("ReportParameter");
+                reader.MoveToAttribute("Name");
+                temp = reader.Value;
+                reader.ReadToFollowing("DataType");
+                if (reader.NodeType != XmlNodeType.None)
+                {
+                    RDLParam rp = new RDLParam();
+                    rp.ParameterName = temp;
+                    rp.Datatype = reader.ReadElementContentAsString();
+                    reader.ReadToFollowing("Prompt");
+                    rp.Prompt = reader.ReadElementContentAsString();
+                    l.Add(rp);
+                }
+
+                rptAtt.Parameters = l;
+
+            }
+
+            return rptAtt;
+        }
+    }
+
+    //Parameter Class
+    public class RDLParam
+    {
+        public String ParameterName { get; set; }
+        public String Datatype { get; set; }
+        public String Prompt { get; set; }
+    }
+
+    //Report Attributes Class
+    public class RDLAttributes
+    {
+        public List<RDLParam> Parameters { get; set; }
+        public String DataSourceName { get; set; }
+        public String CommandText { get; set; }
+    }
+}
